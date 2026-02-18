e686512253ee46de95759fda56023536ed94225c
Leo Tomsett <c4010878@hallam.shu.ac.uk>
2026-02-18 16:20:51 +0000

    Changed drap bar to black

--- sponsor_match/src/app/search/page.tsx
+++ sponsor_match/src/app/search/page.tsx
@@ -189,10 +189,10 @@ export default function SearchPage() {
                 step={500}
                 value={maxBudget}
                 onChange={(e) => setMaxBudget(Number(e.target.value))}
-                className="mt-3 w-full"
+                className="mt-3 accent-Black w-full"
               />
 
-              <div className="mt-1 flex justify-between text-xs font-bold">
+              <div className="mt-1 flex justify-between text- text-xs font-bold">
                 <span>£1,000</span>
                 <span>£20,000</span>
               </div>
